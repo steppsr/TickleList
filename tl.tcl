a9457d770433124b2d332332f3a47996374b6111
Steve Stepp <steppsr@gmail.com>
2011-09-20 15:36:55 -0500

    Added sample todo.txt file.

--- tl.tcl
+++ tl.tcl
@@ -2,3 +2,18 @@
 # 
 # Todo.txt implemenation in TCL/Tk
 #
+
+# Read in command-line parameters
+# 
+
+
+#  Slurp up the data file
+set fp [open "todo.txt" r]
+set file_data [read $fp]
+close $fp
+
+#  Process data file
+set data [split $file_data "\n"]
+foreach line $data {
+  # do some line processing here
+}
