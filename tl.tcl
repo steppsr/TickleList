d116c034b1dbdae23e56ce9074054707eaace8a6
Steve Stepp <steppsr@gmail.com>
2011-11-28 16:16:04 -0600

    Added USAGE if no arguments are passed in. Added PROC to slurp the data file.

--- tl.tcl
+++ tl.tcl
@@ -4,16 +4,30 @@
 #
 
 # Read in command-line parameters
-# 
+if {$argc < 1} {
+  # Since there are no arguments passed in, we just show the USAGE and exit.
+	puts "USAGE:" ;# TODO - fill in complete USAGE for command line interface
+	exit -1;
+}
 
+set dir [file dirname [info script]]
+set path_n_file [file join $dir todo.txt]
 
-#  Slurp up the data file
-set fp [open "todo.txt" r]
-set file_data [read $fp]
-close $fp
+set file_data [read_datafile $path_n_file]
 
 #  Process data file
 set data [split $file_data "\n"]
+
 foreach line $data {
-  # do some line processing here
+	# do some line processing here
+	
+	puts [string range $line 0 78] ;# Just the first 78 chars
+}
+
+#  Slurp up the data file
+proc read_datafile {filename} {
+  set fp [open $filename r]
+  set file_data [read $fp]
+	close $fp
+	return $file_data
 }
